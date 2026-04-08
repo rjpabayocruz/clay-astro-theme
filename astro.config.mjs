5a923ee874e1c04ce6f014228eb1e9cb96b2e452
rpabayoc <rpabayoc@uoguelph.ca>
2026-04-07 20:04:13 -0400

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+
 import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://clay-astro-theme.netlify.app',
-    integrations: [
-        sitemap(), 
-        sanity({
-            projectId: import.meta.env.SANITY_PROJECT_ID,
-            dataset: import.meta.env.SANITY_DATASET || 'production',
-            useCdn: true,
-            apiVersion: '2024-04-07',
-        })
-    ],
+    integrations: [sitemap(), sanity()],
 });
