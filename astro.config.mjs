7b249fcd5dc9450742c9c616eb70bc5007e409bc
rpabayoc <rpabayoc@uoguelph.ca>
2026-04-21 20:51:15 -0400

    hybrid updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 import sanity from "@sanity/astro";
+import vercel from "@astrojs/vercel/serverless"; // 1. Import the adapter
 //test something
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid', // or 'server'
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
